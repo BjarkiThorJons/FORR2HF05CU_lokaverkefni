6f5b3535543c862d1d56e983feefbf2e2bf57c95
BjarkiThorJons <bjarkijo262@nemi.tskoli.is>
2017-11-30 19:25:34 +0000

    bardagi ovinur update

--- bardagi.py
+++ bardagi.py
@@ -14,6 +14,7 @@ class bardagi:
         heilsa_2 = self.s2["heilsa"]
         input("Kastaðu tening til að hitta (ýttu á enter)")
         kast_1=randint(1,20)
+        print(kast_1)
         for x in self.e1:
             if self.e1[x]["notun"]=="vopn":
                 dmg_1=self.e1[x]["value"]
@@ -30,11 +31,41 @@ class bardagi:
             return heilsa_2
         else:
             return heilsa_2
+    def bardaginn_ovinur(self):
+        styrkur_1=self.s1["styrkur"]
+        heilsa_2 = self.s2["heilsa"]
+        input("Óvinurinn kastar tening til að hitta")
+        kast_1=randint(1,20)
+        print(kast_1)
+        for x in self.e1:
+            if self.e1[x]["notun"]=="vopn":
+                dmg_1=self.e1[x]["value"]
+        for x in self.e2:
+            if self.e2[x]["notun"]=="vernd":
+                vernd_2=self.e2[x]["value"]
+        print(kast_1,styrkur_1,vernd_2)
+        if kast_1+styrkur_1>=vernd_2:
+            print("Óvinurinn hittir")
+            input("Óvinurinn kastar tening til að meiða (ýttu á enter)")
+            kast_2=randint(1,dmg_1)
+            heilsa_2=heilsa_2-kast_2
+            print("Óvinurinn meiddi fyrir:",kast_2)
+            return heilsa_2
+        else:
+            return heilsa_2
 while True:
     bardaginn=bardagi(character,character_equipment,enemy,enemy_equipment)
     enemy["heilsa"]=bardaginn.bardaginn_notandi()
+    bardaginn=bardagi(enemy,enemy_equipment,character, character_equipment)
     print(enemy)
+    character["heilsa"]=bardaginn.bardaginn_ovinur()
+    print(character)
     if enemy["heilsa"]<=0:
         print("Þú drapst óvininn")
         break
+    elif character["heilsa"]<=0:
+        print("Þú tapaðir")
+        break
+    print("Heilsan þín:",character["heilsa"])
+    print("Heilsa óvinarins:",enemy["heilsa"])
 
