f4c147b38d175430859bd50dd8d300dcabe0e76c
atlioskarsson <atlios219@nemi.tskoli.is>
2017-12-01 11:23:27 +0000

    Character update

--- character.py
+++ character.py
@@ -1,28 +1,28 @@
 class character:
     def __init__(self, nafn, kyn):
-        # Humna
+        # Maður
         if kyn == 1:
-            strength = 10
-            health = 10
-            gold = 100
-            dexterity = 15
-            inventory = {"sheep":{"value": 10, "price": 20}}
+            strength = 6
+            health = 20
+            gold = 1000
+            equipment = {"Járnsverð":{"value":8, "verð":1000, "tegund":"sverð"}, "Járn brynja":{"value":25, "tegund":"brynja"}}
+            inventory = {"vatn":{"value":6,"tegund":"heilsa","magn":2}, "Kjöt":{"value":6, "tegund":"heilsa", "magn":1}}
 
         # Dvergur
         if kyn == 2:
-            strength = 20
+            strength = 10
             health = 30
-            gold = 50
-            dexterity = 10
-            inventory = {"lamb":{"value": 5, "price": 10}}
+            gold = 500
+            equipment = {"Gullexi":{"value":6, "verð":0, "tegund":"sverð"}, "Leður brynja":{"value":15, "tegund":"brynja"}}
+            inventory = {"Öl":{"value":5, "tegund":"heilsa", "magn":1}, "Brauð":{"value":5, "tegund":"heilsa", "magn":1}}
 
         # Álfur
         if kyn == 3:
-            strength = 15
-            health = 20
-            dexterity = 13
-            gold = 75
-            inventory = {"Water":{"value":7, "price": 15},"fire":{"value":3}}
+            strength = 8
+            health = 25
+            gold = 750
+            equipment = {"Spjót":{"value":7, "verð":200, "tegund":"sverð"}, "Brynja":{"value":20, "tegund":"brynja"}}
+            inventory = {"Heilsu flaska":{"value":10,"tegund":"heilsa", "magn":1},"brauð":{"value":5, "tegund":"heilsa", "magn":2}}
 
         self.nafn = nafn
         self.strength = strength
@@ -30,6 +30,7 @@ class character:
         self.gold = gold
         self.dext = dexterity
         self.inventory = inventory
+
     def __str__(self):
         inv = ""
         print(next(iter(self.inventory)))
@@ -45,7 +46,10 @@ class character:
                "Gull: {gold} \n" \
                "Handlagn: {dext} \n" \
                "Birgðir: {inventory}".format(nafn = self.nafn, strength = self.strength, health = self.health, gold = self.gold, dext = self.dext, inventory = inv)
-    def bardagi(self):
+
+    def stats(self):
         stats = {"styrkur":self.strength, "heilsa":self.health, "gold":self.health, "dext": self.dext, "inv":self.inventory}
         return stats
+
+
 
