1bce21cdfa3f874d1dd60ab9372396f9a2fd8fe1
atlioskarsson <atlios219@nemi.tskoli.is>
2017-12-08 12:28:49 +0000

    breytingar til að skýra betur hvað er í gangi

--- bardagi.py
+++ bardagi.py
@@ -1,5 +1,4 @@
 from random import *
-from time import sleep
 
 class bardagi:
     def __init__(self,stats_1,equipment_1,stats_2,equipment_2):
@@ -12,8 +11,10 @@ class bardagi:
         styrkur_1=self.s1["styrkur"]
         heilsa_2 = self.s2["heilsa"]
         input("Kastaðu tening til að hitta (ýttu á enter)")
+        for x in range(9):
+            print("")
+
         kast_1=randint(1,20)
-        print(kast_1)
 
         for x in self.e1:
             if self.e1[x]["tegund"]=="vopn":
@@ -23,13 +24,23 @@ class bardagi:
             if self.e2[x]["tegund"]=="brynja":
                 vernd_2=self.e2[x]["value"]
 
-        print(kast_1,styrkur_1,vernd_2)
+        # Prentar kastið hjá notenda og sýnir vernd hjá óvini
+        utkoma = kast_1 + styrkur_1
+        print("þú kastaðir fyrir {0}\n"
+              "{1} + {0} = {2}\n"
+              "Vernd hans er {3}".format(kast_1,styrkur_1, utkoma,vernd_2))
+
         if kast_1+styrkur_1>=vernd_2:
             print("Þú hittir")
+            print("-----------------------")
             input("Kastaðu tening til að meiða (ýttu á enter)")
+            for x in range(11):
+                print("")
             kast_2=randint(1, dmg_1)
             heilsa_2=heilsa_2-kast_2
             print("Þú meiddir fyrir:", kast_2)
+            print("{0} - {1} = {2}".format(self.s2["heilsa"], kast_2, heilsa_2))
+            print("Óvinur er nú með {0} í heilsu".format(heilsa_2))
             return heilsa_2
 
         else:
@@ -38,10 +49,10 @@ class bardagi:
     def bardaginn_ovinur(self):
         styrkur_1=self.s1["styrkur"]
         heilsa_2 = self.s2["heilsa"]
-        print("Óvinurinn kastar tening til að hitta")
-        sleep(1)
+        input("Óvinurinn kastar tening til að hitta(enter)")
         kast_1=randint(1,20)
-        print("Kast_1", kast_1)
+        for x in range(8):
+            print("")
 
         for x in self.e1:
             if self.e1[x]["tegund"]=="vopn":
@@ -51,11 +62,19 @@ class bardagi:
             if self.e2[x]["tegund"]=="brynja":
                 vernd_2=self.e2[x]["value"]
 
-        print(kast_1,styrkur_1,vernd_2)
+        utkoma = kast_1 + styrkur_1
+        print("Óvinur kastar fyrir {0}\n"
+              "{1} + {0} = {2}\n"
+              "Vernd þin {3}".format(kast_1, styrkur_1, utkoma, vernd_2))
         if kast_1+styrkur_1>=vernd_2:
+
             print("Óvinurinn hittir")
-            input("Óvinurinn kastar tening til að meiða (ýttu á enter)")
-            kast_2=randint(1,dmg_1)
+            print("-----------------")
+            print("Óvinurinn kastar tening til að meiða")
+            for x in range(10):
+                print("")
+
+            kast_2=randint(1, dmg_1)
             heilsa_2=heilsa_2-kast_2
             print("Óvinurinn meiddi fyrir:", kast_2)
             return heilsa_2
--- main_file.py
+++ main_file.py
@@ -1,23 +1,28 @@
 from bardagi import *
 from character import *
+import os
 
 spilari = character("Jón", 1)
 print(spilari)
 enemy={"styrkur":10,"heilsa":20,"dext":10}
-enemy_equipment={"járn öxi":{"value":8,"verd":1200,"tegund":"vopn"},"járn brynja":{"value":2,"verd":750,"tegund":"brynja"}}
+enemy_equipment={"járn öxi":{"value":8,"verd":1200,"tegund":"vopn"},"járn brynja":{"value":6,"verd":750,"tegund":"brynja"}}
 
 while True:
     bardaginn=bardagi(spilari.stats(),spilari.hlutr,enemy,enemy_equipment)
+    ovinur_heilsa = enemy["heilsa"]
+    ovinur_heilsa = str(ovinur_heilsa)
     enemy["heilsa"]=bardaginn.bardaginn_notandi()
     bardaginn = bardagi(enemy, enemy_equipment, spilari.stats(), spilari.hlutr)
-    print(enemy)
     spilari.stats()["heilsa"]=bardaginn.bardaginn_ovinur()
-    print("Högg stig", spilari.stats()["heilsa"])
+
+    print("Heilsa:", spilari.stats()["heilsa"])
     if enemy["heilsa"]<=0:
         print("Þú drapst óvininn")
         break
+
     elif spilari.stats()["heilsa"]<=0:
         print("Þú tapaðir")
         break
-    print("Heilsan þín:",spilari.stats()["heilsa"])
+
     print("Heilsa óvinarins:", enemy["heilsa"])
+    print("-----------------------")
